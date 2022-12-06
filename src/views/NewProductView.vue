1e48bd2ee2f2b478d257a6bd6c468ccdc4401465
Kyle <ksmontville@gmail.com>
2022-12-05 19:01:47 -0500

    Add new products section to landing page.

--- src/views/NewProductView.vue
+++ src/views/NewProductView.vue
@@ -24,16 +24,16 @@ await getData()
 
   <div class="flex flex-col gap-8 min-h-full text-white">
 
-    <p class="header text-center">{{ data.title }}</p>
+    <p v-if="data.title" class="header text-center">{{ data.title }}</p>
 
     <div class="new-products flex flex-row flex-wrap gap-6">
 
-      <div class="product-1">
+      <div v-if="data.product_1" class="product-1">
         <img class="rounded-md" :src="data.product_1[1].value.thumbnail.src" height="100" width="100" :alt="data.product_1[1].value.title">
         <p v-html="data.product_1[0].value"></p>
       </div>
 
-      <div class="product-2">
+      <div v-if="data.product_2.length !== 0" class="product-2">
         <img class="rounded-md" :src="data.product_2[1].value.thumbnail.src" height="100" width="100" :alt="data.product_2[1].value.title">
         <p v-html="data.product_2[0].value"></p>
       </div>
