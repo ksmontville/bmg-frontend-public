36d85435048c51ae804d4d9b04953137b5f362f4
Kyle <ksmontville@gmail.com>
2022-12-20 19:58:10 -0500

    Add animation to navbar icon.

--- src/components/ModalComponent.vue
+++ src/components/ModalComponent.vue
@@ -70,18 +70,6 @@ onMounted(() => {
 
 <style scoped>
 
-.modal-mask {
-  position: fixed;
-  z-index: 0;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: table;
-  transition: opacity 0.3s ease;
-}
-
 .modalWrapperMobile {
   position: absolute;
   top: 50%;
--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -1,15 +1,21 @@
 <script setup>
 import EventsComponent from "../components/EventsComponent.vue"
+import ButtonComponent from "../components/ButtonComponent.vue";
+
+const subToCalendar = () => {
+  window.open(import.meta.env.VITE_CALENDAR_URL)
+}
 
 </script>
 
 
 <template>
 
-  <div class="flex flex-col text-center text-white p-4">
-    <p class="text-4xl">Check out our in-store events!</p>
-    <p class="text-2xl"> Click the desired link for more specific event and registration information.</p>
+  <div class="flex flex-col items-center gap-4 text-center text-white p-4">
+    <p class="text-2xl mt-4">Subscribe to the Black Moon Games event calendar to stay up to date with the latest in-store events.</p>
+    <ButtonComponent button-text="Subscribe to Calendar" button-class="mt-8 bg-blue-900" @click="subToCalendar"></ButtonComponent>
   </div>
+
 
   <div class="p-2">
     <Transition name="fade">
