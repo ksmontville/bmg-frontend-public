c6caede630ee4d83fa94a61b4dcd27169b5fc91e
Kyle <ksmontville@gmail.com>
2023-02-09 11:08:22 -0500

    Add automated maintenance page.

--- src/views/MaintenancePage.vue
+++ src/views/MaintenancePage.vue
@@ -6,7 +6,7 @@
 
 <template>
 
-  <div class="text-center text-5xl text-white">
+  <div class="text-center text-5xl text-white p-8 m-auto">
     We are currently undergoing routine maintenance. This shouldn't take too long, please check back later!
   </div>
 
